ca7f1e7608f39ccfcd7697db371445872bdd862c
User <User@DESKTOP-13D173P>
2025-09-26 19:54:22 +0530

    Update

--- JavaSample/src/Day1/Sample.java
+++ JavaSample/src/Day1/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the Github Session");
+		System.out.println("New line");
 
 	}
 
